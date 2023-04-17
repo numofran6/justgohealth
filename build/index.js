925d1b4a3abf3ef98172575959fe836574dd0bc3
boypaida12 <baddysackey5@gmail.com>
2023-04-17 23:40:38 +0000

    updated hero-section

---
+++ build/index.js
@@ -0,0 +1,32 @@
+const jobTitles = [
+    'Physician-Scientists',
+    'Medical Curators',
+    'Health Researchers',
+    'Patient Advocates',
+    'Health Organizations',
+    'Medical Experts',
+  ];
+  
+  const jobTitleElem = document.getElementById('job-title');
+  let currentJobTitleIndex = 0;
+  
+  
+  setInterval(() => {
+    
+    setTimeout(() => {
+      currentJobTitleIndex = (currentJobTitleIndex + 1) % jobTitles.length;
+      jobTitleElem.textContent = jobTitles[currentJobTitleIndex];
+      
+      if (currentJobTitleIndex === 1 || currentJobTitleIndex === 5) {
+        jobTitleElem.classList.add('justGo-blue');
+        jobTitleElem.classList.remove('justGo-purple', 'justGo-green');
+      } else if (currentJobTitleIndex === 0 || currentJobTitleIndex === 3) {
+        jobTitleElem.classList.add('justGo-green');
+        jobTitleElem.classList.remove('justGo-purple', 'justGo-blue');
+      } else {
+        jobTitleElem.classList.add('justGo-purple');
+        jobTitleElem.classList.remove('justGo-blue', 'justGo-green');
+      }
+    }, 500);
+  }, 1500);
+  
